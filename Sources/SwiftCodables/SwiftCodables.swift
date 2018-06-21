ca0ddae4625ad88920f28f2f4b0d18c62d43efdb
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-06-21 14:14:25 -0400

    Added string to device limit struct

--- Sources/SwiftCodables/SwiftCodables.swift
+++ Sources/SwiftCodables/SwiftCodables.swift
@@ -166,7 +166,6 @@ public struct BiqDeviceLimit: Codable {
 	public let limitValue: Float
 	public let limitValueString: String?
 	public var type: BiqDeviceLimitType? { return BiqDeviceLimitType(rawValue: limitType) }
-	
 	public init(userId u: UserId, deviceId d: DeviceURN, limitType t: BiqDeviceLimitType, limitValue v: Float = 0.0, limitValueString vs: String? = nil) {
 		userId = u
 		deviceId = d
